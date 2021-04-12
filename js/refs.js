34897e4fa4a2be878acfd9fa679e1363d9abd276
KristinaY-87 <72267606+KristinaY-87@users.noreply.github.com>
2021-04-12 23:51:08 +0300

    обновляет код

---
+++ js/refs.js
@@ -0,0 +1,9 @@
+const refs = {
+    timerId: document.querySelector('#timer-1'),
+    days: document.querySelector('[data-value="days"]'),
+    hours: document.querySelector('[ data-value="hours"]'),
+    mins: document.querySelector('[data-value="mins"]'),
+    secs: document.querySelector('[data-value="secs"]'),
+};
+        
+export default refs
